53804eac7558b06595b8b7457bc3ec0c782b7f8a
Glazelf <gideon.steenbergen@gmail.com>
2023-02-25 16:47:52 +0100

    import basic connectivity
    from https://github.com/Glazelf/Tableturf-idler/blob/master/index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const fs = require("fs");
+const net = require("net");
 const readline = require('readline-sync');
 const config = require('./config.json');
 
-let subgameInput = readline.question("Which subgame do you want to play?\nOptions: \"crackityhack\", \"eggcatcher\", \"samuraikirby\"\n");
+let allowedSubgames = ["crackityhack", "eggcatcher", "samuraikirby"];
+let subgameInput = readline.question(`Which subgame do you want to play?\nOptions: ${allowedSubgames.join(", ")}\n`);
+if (!allowedSubgames.includes(subgameInput)) return console.log("That subgame isn't available (yet)!");
 let difficultyInput = readline.question("What difficulty do you want to play?\nOptions: 1, 2, 3\n");
+
+const port = config.port;
+const host = config.ip;
+const conn = net.createConnection(port, host);
+conn.setEncoding("utf-8"); // sends all commands as utf-8 (same as .encode())
+
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
