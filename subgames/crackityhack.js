095e7f20b05a20b66f867567c26607a6b849175b
Glazelf <gideon.steenbergen@gmail.com>
2023-02-25 18:00:50 +0100

    switch tree

--- subgames/crackityhack.js
+++ subgames/crackityhack.js
@@ -1,3 +1,11 @@
-exports.run = async ({ connection, sleep, difficulty }) => {
-    return console.log("Starting Crackity Hack loop...");
+exports.run = async ({ conn, sleep, difficulty }) => {
+    console.log("Starting Crackity Hack loop...");
+    switch (difficulty) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+    };
 };
